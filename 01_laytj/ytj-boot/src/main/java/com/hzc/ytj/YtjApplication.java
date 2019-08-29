91b552a0ee9e903d9c31bedd1af695844f74331c
huangzhicheng <15104079201@163.com>
2019-08-29 17:42:57 +0800

    提交

---
+++ 01_laytj/ytj-boot/src/main/java/com/hzc/ytj/YtjApplication.java
@@ -0,0 +1,13 @@
+package com.hzc.ytj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YtjApplication.class, args);
+    }
+
+}
---
+++ 01_laytj/ytj-boot/src/test/java/com/hzc/ytj/YtjApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hzc.ytj;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YtjApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
